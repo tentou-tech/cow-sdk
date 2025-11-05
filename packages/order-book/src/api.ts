86a78a03c33d015dc4847d1f38360fd01c360a88
phanhoc <phanhocit@gmail.com>
2025-11-05 14:08:36 +0700

    update url dev

--- packages/order-book/src/api.ts
+++ packages/order-book/src/api.ts
@@ -32,8 +32,8 @@ import { transformOrder } from './transformOrder'
 import { EnrichedOrder } from './types'
 
 const PROD_BASE_URL = 'https://api.cow.fi'
-const STAGING_BASE_URL = 'https://barn.api.cow.fi'
-const LOCAL_BASE_URL = 'http://localhost:8080'
+const STAGING_BASE_URL = 'http://34.146.110.209:8080'
+// const LOCAL_BASE_URL = 'http://localhost:8080'
 
 /**
  * An object containing *production* environment base URLs for each supported `chainId`.
@@ -65,7 +65,7 @@ export const ORDER_BOOK_STAGING_CONFIG: ApiBaseUrls = {
   [SupportedChainId.AVALANCHE]: `${STAGING_BASE_URL}/avalanche`,
   [SupportedChainId.LENS]: `${STAGING_BASE_URL}/lens`,
   [SupportedChainId.BNB]: `${STAGING_BASE_URL}/bnb`,
-  [SupportedChainId.HYPER_EVM_TESTNET]: `${LOCAL_BASE_URL}`,
+  [SupportedChainId.HYPER_EVM_TESTNET]: `${STAGING_BASE_URL}`,
 }
 
 function cleanObjectFromUndefinedValues(obj: Record<string, string>): typeof obj {
